99cceb0f93b566a77b65fc8f8d00981d87d809c4
Anna <zoloto86@list.ru>
2022-11-20 20:07:43 +0700

    add service

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public int calculate(int coast) {
+        int ticket = coast;
+        int miles = 20 * 1;
+        int bonusMiles = ticket / miles;
+
+        return bonusMiles;
+
+    }
+}
